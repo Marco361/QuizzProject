76594ad6f11b328a8c58986bf1ef385d0b361836
Marek Giel <marek.giel@wp.pl>
2020-11-01 23:50:43 +0100

    Add new files

---
+++ back.py
@@ -0,0 +1,13 @@
+import csv
+
+with open('problems.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Kolumny to {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\t dla pytania {row[0]} odzpowiedzia jest {row[1]}')
+            line_count += 1
+    print(f'Processed {line_count} lines.')
